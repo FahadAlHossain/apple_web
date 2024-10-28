ccbc6493135f28dcfd890314ba9c41f9ace9c789
FahadAlHossain <fahadalhossain5@gmail.com>
2024-10-28 14:18:25 +0600

    nav,highlights add

---
+++ my-project/src/components/VideoCarousel.jsx
@@ -0,0 +1,14 @@
+import { hightlightsSlides } from "../constants"
+
+
+const VideoCarousel = () => {
+  return (
+    <>
+    <div className="flex items-center">
+        {hightlightsSlides}
+    </div>
+    </>
+  )
+}
+
+export default VideoCarousel
